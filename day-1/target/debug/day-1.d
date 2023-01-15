665c34b1c1bbb0efb6e2a99fc63d88c59c07674f
Onur Ereren <onur.ereren998@gmail.com>
2023-01-16 00:05:01 +0100

    added gitignore

--- day-1/target/debug/day-1.d
+++
@@ -1,1 +0,0 @@
-D:\IdeaProjects\adventofcode-2022\Day-1\target\debug\day-1.exe: D:\IdeaProjects\adventofcode-2022\Day-1\src\main.rs
--- day-1/target/debug/deps/day_1-6c45dbbfdc1614ca.d
+++
@@ -1,5 +0,0 @@
-D:\IdeaProjects\adventofcode-2022\Day-1\target\debug\deps\day_1-6c45dbbfdc1614ca.exe: src\main.rs
-
-D:\IdeaProjects\adventofcode-2022\Day-1\target\debug\deps\day_1-6c45dbbfdc1614ca.d: src\main.rs
-
-src\main.rs:
