3f49cba05c565ac00009c63c87ebdeed03570206
oererenn <onur.ereren998@gmail.com>
2023-02-27 21:19:28 +0100

    day-2

---
+++ day-2/src/main.rs
@@ -0,0 +1,61 @@
+use std::error::Error;
+
+fn main() {
+    for round in include_str!("input.txt").lines().map(|line| line.trim()) {
+        println!("{round:?}");
+    }
+}
+
+#[derive(Debug)]
+struct Round {
+    Player1: Move,
+    Player2: Move,
+}
+
+#[derive(Debug)]
+enum Outcome {
+    Win,
+    Draw,
+    Loss,
+}
+
+#[derive(Debug)]
+enum Move {
+    Rock,
+    Paper,
+    Scissors,
+}
+
+impl Move {
+    /// How many points do we get for picking that move?
+    fn inherent_points(self) -> usize {
+        match self {
+            Move::Rock => 1,
+            Move::Paper => 2,
+            Move::Scissors => 3,
+        }
+    }
+    
+    fn compare(c: char) -> Result<Move, (&'static str, &'static str)>{
+        match c {
+            'A' | 'X' => Ok(Move::Rock),
+            'B' | 'Y' => Ok(Move::Paper),
+            'C' | 'Z' => Ok(Move::Scissors),
+            _ => Err(("Invalid move", "Move must be A, B, C, X, Y, or Z")),
+        }
+    }
+
+    fn outcome(self, theirs: Move) -> Outcome {
+        match (self, theirs) {
+            (Move::Rock, Move::Rock) => Outcome::Draw,
+            (Move::Rock, Move::Paper) => Outcome::Loss,
+            (Move::Rock, Move::Scissors) => Outcome::Win,
+            (Move::Paper, Move::Rock) => Outcome::Win,
+            (Move::Paper, Move::Paper) => Outcome::Draw,
+            (Move::Paper, Move::Scissors) => Outcome::Loss,
+            (Move::Scissors, Move::Rock) => Outcome::Loss,
+            (Move::Scissors, Move::Paper) => Outcome::Win,
+            (Move::Scissors, Move::Scissors) => Outcome::Draw,
+        }
+    }
+}
