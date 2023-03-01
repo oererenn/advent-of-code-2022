07c7ae2b7b7a9a1bf36f3c0e839b241479936726
oererenn <onur.ereren998@gmail.com>
2023-03-02 00:06:44 +0100

    added day-2 second part

--- day-2/src/main.rs
+++ day-2/src/main.rs
@@ -1,26 +1,64 @@
 use std::fs::read_to_string;
-const FILENAME: &str = "src/input.txt"; 
+const FILENAME: &str = "src/input.txt";
 fn main() {
-calculate_total_points(FILENAME);
+    calculate_total_points(FILENAME);
+    counter_play(FILENAME);
 }
 
 fn calculate_total_points(filename: &str) -> usize {
     let mut result = 0;
-    for round in read_to_string(filename).unwrap().lines().map(|line| line.trim()) {
-        let round =  round.chars().filter(|c| !c.is_whitespace()).collect::<String>();
+    for round in read_to_string(filename)
+        .unwrap()
+        .lines()
+        .map(|line| line.trim())
+    {
+        let round = round
+            .chars()
+            .filter(|c| !c.is_whitespace())
+            .collect::<String>();
         let round = Round {
             player_1: Move::compare(round.chars().nth(0).unwrap()).unwrap(),
             player_2: Move::compare(round.chars().nth(1).unwrap()).unwrap(),
         };
-        let move_selection_point = round.player_2.inherent_points();
+        let move_selection_point = round.player_2.calculate_move_points();
         let outcome = round.player_1.outcome(round.player_2);
 
         let outcome_point = outcome.points();
         let final_point = move_selection_point + outcome_point;
         result += final_point;
-        println!("{:?}",final_point);
     }
-    println!("{:?}",result);
+    println!("{:?}", result);
+    result
+}
+
+fn counter_play(filename: &str) -> usize {
+    let mut result = 0;
+    for round in read_to_string(filename)
+        .unwrap()
+        .lines()
+        .map(|line| line.trim())
+    {
+        let round = round
+            .chars()
+            .filter(|c| !c.is_whitespace())
+            .collect::<String>();
+        let round = Round {
+            player_1: Move::compare(round.chars().nth(0).unwrap()).unwrap(),
+            player_2: Move::compare(round.chars().nth(1).unwrap()).unwrap(),
+        };
+        let counter_outcome = round.player_2.calculate_round();
+
+        let counter_move = match counter_outcome {
+            Outcome::Win => round.player_1.play_winning_move(),
+            Outcome::Draw => round.player_1.play_draw_move(),
+            Outcome::Loss => round.player_1.play_losing_move(),
+        };
+
+        let move_points = counter_move.calculate_move_points();
+        let round_points = counter_outcome.points();
+        result += move_points + round_points;
+    }
+    println!("{:?}", result);
     result
 }
 
@@ -46,20 +84,52 @@ enum Move {
 
 impl Move {
     /// How many points do we get for picking that move?
-    fn inherent_points(&self) -> usize {
+    fn calculate_move_points(&self) -> usize {
         match self {
             Move::Rock => 1,
             Move::Paper => 2,
             Move::Scissors => 3,
         }
     }
-    
-    fn compare(c: char) -> Result<Move, (&'static str, &'static str)>{
+
+    fn compare(c: char) -> Result<Move, (&'static str, &'static str)> {
         match c {
             'A' | 'X' => Ok(Move::Rock),
             'B' | 'Y' => Ok(Move::Paper),
             'C' | 'Z' => Ok(Move::Scissors),
             _ => Err(("Invalid move", "Move must be A, B, C, X, Y, or Z")),
+        }
+    }
+
+    fn calculate_round(self) -> Outcome {
+        match self {
+            Move::Rock => Outcome::Loss,
+            Move::Paper => Outcome::Draw,
+            Move::Scissors => Outcome::Win,
+        }
+    }
+
+    fn play_winning_move(self) -> Move {
+        match self {
+            Move::Rock => Move::Paper,
+            Move::Paper => Move::Scissors,
+            Move::Scissors => Move::Rock,
+        }
+    }
+
+    fn play_losing_move(self) -> Move {
+        match self {
+            Move::Rock => Move::Scissors,
+            Move::Paper => Move::Rock,
+            Move::Scissors => Move::Paper,
+        }
+    }
+
+    fn play_draw_move(self) -> Move {
+        match self {
+            Move::Rock => Move::Rock,
+            Move::Paper => Move::Paper,
+            Move::Scissors => Move::Scissors,
         }
     }
 
@@ -90,11 +160,17 @@ impl Outcome {
 
 #[cfg(test)]
 mod tests {
-    use crate::calculate_total_points;
+    use crate::{calculate_total_points, counter_play};
 
     #[test]
     fn test_total_points() {
         let points = calculate_total_points("src/test.txt");
         assert_eq!(points, 15);
+    }
+
+    #[test]
+    fn test_counter_play() {
+        let points = counter_play("src/test.txt");
+        assert_eq!(points, 12);
     }
 }
